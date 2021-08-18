7ed78876ae26b5c83a2fa838a211543f7b7ba0be
zachary chiodini <zacharybchiodini@gmail.com>
2021-08-18 16:50:41 -0400

    New directory

---
+++ vue-web-ui/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ag-grid-vue style="width: 500px; height: 500px;"
+    class="ag-theme-alpine"
+    :columnDefs="columnDefs"
+    :rowData="rowData">
+  </ag-grid-vue>
+</template>
+
+<script>
+import { AgGridVue } from 'ag-grid-vue3'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      columnDefs: null,
+      rowData: null
+    }
+  },
+  components: {
+    AgGridVue
+  },
+  beforeMount () {
+    this.columnDefs = []
+    this.rowData = [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ]
+    for (const val of this.rowData[0]) {
+      this.columnDefs.push({field: val})
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
+</style>
